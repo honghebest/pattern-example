5a08fc7d3990023638e106bfaa19fce9fee1ee82
hongghe <364930855@qq.com>
2018-07-14 19:25:48 +0800

    Initial commit

---
+++ src/main/java/com/hongghe/patternexample/PatternExampleApplication.java
@@ -0,0 +1,12 @@
+package com.hongghe.patternexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatternExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatternExampleApplication.class, args);
+	}
+}
