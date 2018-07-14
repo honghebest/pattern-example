a3e28b763030bde837e568398a8e695fb3c23256
hongghe <364930855@qq.com>
2018-07-14 20:49:25 +0800

    singleton mode.

---
+++ src/main/java/com/hongghe/patternexample/singleton/LazySingleton.java
@@ -0,0 +1,17 @@
+package com.hongghe.patternexample.singleton;
+
+/**
+ * 懒汉式
+ *
+ * @author hongghe 07/2018
+ */
+public class LazySingleton {
+
+    private LazySingleton lazySingleton = null;
+
+    private LazySingleton() {}
+
+    public LazySingleton getInstance() {
+        return new LazySingleton();
+    }
+}
