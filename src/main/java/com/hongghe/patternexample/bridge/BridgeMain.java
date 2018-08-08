5f410c20c8d502415fae076268d3b8ff0a066fb6
hongghe <364930855@qq.com>
2018-08-08 23:55:03 +0800

    Bridge.

---
+++ src/main/java/com/hongghe/patternexample/bridge/BridgeMain.java
@@ -0,0 +1,18 @@
+package com.hongghe.patternexample.bridge;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 使用
+ *
+ * @author hongghe 08/08/2018
+ */
+public class BridgeMain {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BridgeMain.class);
+
+    public static void main(String[] args) {
+
+    }
+}
