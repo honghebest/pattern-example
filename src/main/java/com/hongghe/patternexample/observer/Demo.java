9fec9268ee450117b9b112af07baa46b5d08385b
hongghe <364930855@qq.com>
2019-01-18 21:22:17 +0800

    change name of pattern.

--- src/main/java/com/hongghe/patternexample/observer/Demo.java
+++ src/main/java/com/hongghe/patternexample/observer/Demo.java
@@ -3,15 +3,15 @@ package com.hongghe.patternexample.observer;
 import com.hongghe.patternexample.observer.generic.GHobbits;
 import com.hongghe.patternexample.observer.generic.GWeather;
 import com.hongghe.patternexample.observer.generic.Race;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * @author hongghe 2018/8/17
  */
+@Slf4j
 public class Demo {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(Demo.class);
 
     /**
      * Program entry point
@@ -30,7 +30,7 @@ public class Demo {
         weather.timePasses();
 
         // Generic observer inspired by Java Generics and Collection by Naftalin & Wadler
-        LOGGER.info("--Running generic version--");
+        log.info("--Running generic version--");
         GWeather gWeather = new GWeather();
         //gWeather.addObserver(new Race());
         gWeather.addObserver(new GHobbits());
