e4d972fe52a9a32e69d978cdc2bfe0299b745f71
hongghe <364930855@qq.com>
2019-01-18 21:21:45 +0800

    change name of pattern.

--- src/main/java/com/hongghe/patternexample/strategy/Demo.java
+++ src/main/java/com/hongghe/patternexample/strategy/Demo.java
@@ -1,14 +1,12 @@
 package com.hongghe.patternexample.strategy;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author hongghe 2018/8/17
  */
+@Slf4j
 public class Demo {
-    private static final Logger LOGGER = LoggerFactory.getLogger(Demo.class);
-
     /**
      * Program entry point
      *
@@ -16,27 +14,27 @@ public class Demo {
      */
     public static void main(String[] args) {
         // GoF Strategy pattern
-        LOGGER.info("Green dragon spotted ahead!");
+        log.info("Green dragon spotted ahead!");
         DragonSlayer dragonSlayer = new DragonSlayer(new MeleeStrategy());
         dragonSlayer.goToBattle();
-        LOGGER.info("Red dragon emerges.");
+        log.info("Red dragon emerges.");
         dragonSlayer.changeStrategy(new ProjectileStrategy());
         dragonSlayer.goToBattle();
-        LOGGER.info("Black dragon lands before you.");
+        log.info("Black dragon lands before you.");
         dragonSlayer.changeStrategy(new SpellStrategy());
         dragonSlayer.goToBattle();
 
         // Java 8 Strategy pattern
-        LOGGER.info("Green dragon spotted ahead!");
+        log.info("Green dragon spotted ahead!");
         dragonSlayer = new DragonSlayer(
-                () -> LOGGER.info("With your Excalibur you severe the dragon's head!"));
+                () -> log.info("With your Excalibur you severe the dragon's head!"));
         dragonSlayer.goToBattle();
-        LOGGER.info("Red dragon emerges.");
-        dragonSlayer.changeStrategy(() -> LOGGER.info(
+        log.info("Red dragon emerges.");
+        dragonSlayer.changeStrategy(() -> log.info(
                 "You shoot the dragon with the magical crossbow and it falls dead on the ground!"));
         dragonSlayer.goToBattle();
-        LOGGER.info("Black dragon lands before you.");
-        dragonSlayer.changeStrategy(() -> LOGGER.info(
+        log.info("Black dragon lands before you.");
+        dragonSlayer.changeStrategy(() -> log.info(
                 "You cast the spell of disintegration and the dragon vaporizes in a pile of dust!"));
         dragonSlayer.goToBattle();
     }
