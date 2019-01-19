4878884be11d44f892dfb365392ab94ed9ebfefa
hongghe <364930855@qq.com>
2019-01-19 13:53:27 +0800

    change name of pattern.

--- src/main/java/com/hongghe/patternexample/builder/PackageInfo.java
+++
@@ -1,7 +0,0 @@
-package com.hongghe.patternexample.builder;
-
-/**
- * Builder的模式的额设置
- *
- * @author hongghe 07/08/2018
- */
--- src/main/java/com/hongghe/patternexample/builder/SuperBuilderDemo.java
+++ src/main/java/com/hongghe/patternexample/builder/SuperBuilderDemo.java
@@ -1,12 +1,7 @@
 package com.hongghe.patternexample.builder;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class SuperBuilderDemo extends BuilderDemo {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(SuperBuilderDemo.class);
-
+    
     public void getBuilderName() {
         return;
     }
