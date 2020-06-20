207df70ebabb7cce5f0efc1047d9dc42762e625d
hongghe <364930855@qq.com>
2020-06-20 20:28:51 +0800

    [design]

--- src/main/java/com/hongghe/patternexample/observer/WeatherObserver.java
+++ src/main/java/com/hongghe/patternexample/observer/WeatherObserver.java
@@ -4,5 +4,11 @@ package com.hongghe.patternexample.observer;
  * @author hongghe 2018/8/17
  */
 public interface WeatherObserver {
+
+    /**
+     * 更新操作
+     *
+     * @param currentWeather current weather
+     */
     void update(WeatherType currentWeather);
 }
